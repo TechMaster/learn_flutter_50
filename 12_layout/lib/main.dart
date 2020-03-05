226c8720137b3f3fa90dd1c08f357ed42c70aae0
techmaster <cuong@techmaster.vn>
2020-03-05 12:04:37 +0700

    12_layout

--- 12_layout/lib/main.dart
+++ 12_layout/lib/main.dart
@@ -7,76 +7,77 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(        
-        body: Center(child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.network("https://images.pexels.com/photos/3767403/pexels-photo-3767403.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"),
-              SizedBox(height: 20,),
-              Text("Romantic Couples", style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic),),
-              SizedBox(height: 20,),
-            /*  Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[                
-                CustomPaint(painter: DrawCircle(Colors.grey[500], 10)),
-                SizedBox(width: 30),
-                CustomPaint(painter: DrawCircle(Colors.black87, 10)),
-                SizedBox(width: 30),
-                CustomPaint(painter: DrawCircle(Colors.grey[500], 10)),
-                SizedBox(width: 30),
-                CustomPaint(painter: DrawCircle(Colors.grey[500], 10))
-              ],)*/
-              DotArray(6, 2)
-            ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Slide Show'),
+        ),
+        body: Center(
+          child: Container(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.network("https://images.pexels.com/photos/3767403/pexels-photo-3767403.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"),
+                SizedBox(height: 15,),
+                Text("Romantic Couples", style: TextStyle(fontSize: 20, fontStyle: FontStyle.italic)),
+                SizedBox(height: 15,),
+                /*Row(children: <Widget>[
+                  CustomPaint(painter: DrawCircle(false)),
+                  SizedBox(width: 20),
+                  CustomPaint(painter: DrawCircle(false)),
+                  SizedBox(width: 20),
+                  CustomPaint(painter: DrawCircle(true)),
+                  SizedBox(width: 20),
+                  CustomPaint(painter: DrawCircle(false)),
+                  SizedBox(width: 20),
+                  CustomPaint(painter: DrawCircle(false)),
+
+                ], mainAxisAlignment: MainAxisAlignment.center,)*/
+                DotArray(7, 0)
+              ],
+            )
           ),
-        ))
-
+        ),
       ),
-    );
-  }
-}
-class DotArray extends StatelessWidget {
-  final int num;
-  final int selectIndex;
-  const DotArray(this.num, this.selectIndex);
-
-  @override
-  Widget build(BuildContext context) {
-    var childrend = <Widget>[];
-    for (var i = 0; i < num; i++) {
-      childrend.add(CustomPaint(painter: DrawCircle(i==selectIndex ? Colors.black87:Colors.grey[500], 8)));
-      if (i < num-1) {
-        childrend.add(SizedBox(width: 30));
-      }
-    }
-    return Container(
-      child: Row(children: childrend,
-      mainAxisAlignment: MainAxisAlignment.center,),
     );
   }
 }
 
 class DrawCircle extends CustomPainter {
   Paint _paint;
-  Color color;
-  double radius;
-  DrawCircle(Color color, double radius) {
-    this.color = color;
-    this.radius = radius;
+  final bool selected;
+  DrawCircle(this.selected) {
     _paint = Paint()
-      ..color = color      
+      ..color = selected? Colors.black87 : Colors.grey[500]
       ..style = PaintingStyle.fill;
   }
 
   @override
   void paint(Canvas canvas, Size size) {
-    canvas.drawCircle(Offset(0,0), this.radius, _paint);
+     canvas.drawCircle(Offset(0.0, 0.0), 8, _paint);
   }
 
   @override
   bool shouldRepaint(CustomPainter oldDelegate) {
     return false;
+  }
+}
+
+class DotArray extends StatelessWidget {
+  final int numDot;
+  final int positionSelecteDot;
+  const DotArray(this.numDot, this.positionSelecteDot);
+
+  @override
+  Widget build(BuildContext context) {
+    var dotarray =  <Widget>[];
+    for (var i = 0; i < numDot; i++) {
+      dotarray.add(CustomPaint(painter: DrawCircle(i==positionSelecteDot)));
+      if (i < numDot-1) {
+        dotarray.add(SizedBox(width: 20));
+      }      
+    }
+
+    return Row (children: dotarray, 
+    mainAxisAlignment: MainAxisAlignment.center);
   }
 }
